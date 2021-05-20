f0bc979c9a2efff7083672ba1a918f6adb033b4c
Vladyslav <65062517+VladyslavVasylenko@users.noreply.github.com>
2021-05-20 21:16:17 +0300

    test

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+const textStyle = {
+  color: 'red',
+}
+
+const textStylesl = {
+  color: 'green',
+}
+
+const textStyles = {
+  color: '#03f',
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={textStyles}>
+          Edit <code style={textStyle}>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={textStylesl}
         >
           Learn React
         </a>
